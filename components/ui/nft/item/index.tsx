e796a47880049267d42aade87293cde93029c6c3
igor Koniukhov <ikoniukhov@gmail.com>
2022-09-23 23:45:49 +0300

    remove unnecessary styles

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -74,9 +74,9 @@ const NftItem: FunctionComponent<NftItemProps> = ({item, buyNft}) => {
                             buyNft(item.tokenId, item.price);
                         }}
                         type="button"
-                        className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed mr-2 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed mr-2 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                        Buy
+                        Buy Nft
                     </button>
                     <button
                         type="button"
