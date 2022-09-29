1d83930f7fd35a09be01d0231b774367d39db4ba
igor Koniukhov <ikoniukhov@gmail.com>
2022-09-29 14:13:34 +0300

    changed styles

--- components/ui/nft/item/index.tsx
+++ components/ui/nft/item/index.tsx
@@ -78,27 +78,47 @@ const NftItem: FunctionComponent<NftItemProps> = ({item, buyNft}) => {
                         type="button"
                         className="disabled:bg-slate-50
                          disabled:text-slate-500
-                          disabled:border-slate-200
-                           disabled:shadow-none
-                            disabled:cursor-not-allowed
-                             mr-2 inline-flex
-                              items-center
-                               px-4 py-2 border
-                                border-transparent
-                                 text-base font-medium
-                                 rounded-md shadow-sm
-                                  text-white bg-yellow-600
-                                   hover:bg-grey-700
-                                    focus:outline-none
-                                     focus:ring-2
-                                      focus:ring-offset-2
-                                       focus:ring-yellow-500"
+                         disabled:border-slate-200
+                         disabled:shadow-none
+                         disabled:cursor-not-allowed
+                         mr-2 inline-flex
+                         items-center
+                         px-4 py-2 border
+                         border-transparent
+                         text-base font-medium
+                         rounded-md shadow-sm
+                         text-white bg-yellow-600
+                         hover:bg-grey-700
+                         focus:outline-none
+                         focus:ring-2
+                         focus:ring-offset-2
+                         focus:ring-yellow-500"
                     >
                         Buy Nft
                     </button>
                     <button
                         type="button"
-                        className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="disabled:bg-slate-50
+                         disabled:text-slate-500
+                         disabled:border-slate-200
+                         disabled:shadow-none
+                         disabled:cursor-not-allowed
+                         inline-flex i
+                         tems-center
+                         px-4 py-2
+                         border
+                         border-gray-300
+                         shadow-sm
+                         text-base
+                         font-medium
+                         rounded-md
+                         text-gray-700
+                         bg-white
+                         hover:bg-gray-50
+                         focus:outline-none
+                         focus:ring-2
+                         focus:ring-offset-2
+                         focus:ring-indigo-500"
                     >
                         Preview
                     </button>
